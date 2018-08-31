0d8d25e58c8562e56ee73ae24ec4b9d6586d9848
beercafeguy <hemchandra@outlook.com>
2018-08-31 22:54:20 +0530

    updated the test class for lazy singleton

--- singleton-pattern/src/test/java/com/beercafeguy/design/AppTest.java
+++ singleton-pattern/src/test/java/com/beercafeguy/design/AppTest.java
@@ -1,20 +1,29 @@
 package com.beercafeguy.design;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
+import com.beercafeguy.design.singleton.Singleton;
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+
+        assertTrue(true);
+    }
+
+    @Test
+    public void testLazySingleton() {
+        Singleton s1=Singleton.getSingleInstance();
+        Singleton s2=Singleton.getSingleInstance();
+        assertEquals(s1,s2);
     }
 }
